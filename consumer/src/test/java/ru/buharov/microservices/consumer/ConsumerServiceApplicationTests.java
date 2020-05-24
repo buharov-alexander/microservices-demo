1b1ac91e00e99b43a6264cc5cb8275de4bb0d0c9
buharov-alexander <buharovsasha@yandex.ru>
2020-05-24 13:42:54 +0700

    create consumer project

---
+++ consumer/src/test/java/ru/buharov/microservices/consumer/ConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.buharov.microservices.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
