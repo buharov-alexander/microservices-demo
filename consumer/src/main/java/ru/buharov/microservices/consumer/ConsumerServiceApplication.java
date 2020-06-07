afbd7cc0d92a7ff78133b63487685d92a707d235
buharov-alexander <buharovsasha@yandex.ru>
2020-06-07 22:57:16 +0700

    consumer: init listener

--- consumer/src/main/java/ru/buharov/microservices/consumer/ConsumerServiceApplication.java
+++ consumer/src/main/java/ru/buharov/microservices/consumer/ConsumerServiceApplication.java
@@ -2,6 +2,8 @@ package ru.buharov.microservices.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.buharov.microservices.consumer.kafka.MessageListener;
 
 @SpringBootApplication
 public class ConsumerServiceApplication {
@@ -10,4 +12,8 @@ public class ConsumerServiceApplication {
         SpringApplication.run(ConsumerServiceApplication.class, args);
     }
 
+    @Bean
+    public MessageListener messageListener() {
+        return new MessageListener();
+    }
 }
