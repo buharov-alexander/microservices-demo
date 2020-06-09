1b3e918354cf657c182d6add6ffd55913087f13e
buharov-alexander <buharovsasha@yandex.ru>
2020-06-09 21:25:15 +0700

    consumer: save listened jokes

--- consumer/src/main/java/ru/buharov/microservices/consumer/kafka/MessageListener.java
+++ consumer/src/main/java/ru/buharov/microservices/consumer/kafka/MessageListener.java
@@ -1,14 +1,20 @@
 package ru.buharov.microservices.consumer.kafka;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import ru.buharov.microservices.common.dto.JokeDTO;
+import ru.buharov.microservices.consumer.jokes.service.JokeService;
 
 @Slf4j
 public class MessageListener {
 
+    @Autowired
+    private JokeService jokeService;
+
     @KafkaListener(topics = "${kafka.topic.name}", containerFactory = "kafkaListenerContainerFactory")
     public void listener(JokeDTO jokeDTO) {
         log.info("Recieved message: {}", jokeDTO);
+        jokeService.saveJoke(jokeDTO);
     }
 }
